d600e04c5882f1807c80de89e9dbdc30be98d1a0
Obaid <mrubaid56@gmail.com>
2025-04-28 14:57:09 +0530

    Basics

---
+++ Js/basic.js
@@ -0,0 +1,16 @@
+// Constants and variables
+
+const userName = " Obaid "
+// userName = " syed "  will through error
+
+let userName2 = " Umaid "
+// can be changed
+userName2 = "Umaid sb" 
+
+var middleName = " Shakeel"
+// can be changed also 
+middleName = "Shakeel Ahmad"
+
+
+// prefered is let over var because of scope issues
+console.table({userName, userName2, middleName });
